dcc153bbbb05c8fe16108ec92083e9fc09a055d2
Nicholas Farr <nickfarr10@gmail.com>
2025-03-31 23:19:32 -0400

    created 5 catch2 tests for project checkin

--- test/test.cpp
+++ test/test.cpp
@@ -1,76 +1,99 @@
 #include <catch2/catch_test_macros.hpp>
 #include <iostream>
-
-// change if you choose to use a different header name
 #include "AdjacencyList.h"
 
 using namespace std;
 
-// the syntax for defining a test is below. It is important for the name to be
-// unique, but you can group multiple tests with [tags]. A test can have
-// [multiple][tags] using that syntax.
-TEST_CASE("Example Test Name - Change me!", "[tag]") {
-  // instantiate any class members that you need to test here
-  int one = 1;
-
-  // anything that evaluates to false in a REQUIRE block will result in a
-  // failing test
-  REQUIRE(one == 0); // fix me!
-
-  // all REQUIRE blocks must evaluate to true for the whole test to pass
-  REQUIRE(false); // also fix me!
-}
-
-TEST_CASE("Test 2", "[tag]") {
-  // you can also use "sections" to share setup code between tests, for example:
-  int one = 1;
-
-  SECTION("num is 2") {
-    int num = one + 1;
-    REQUIRE(num == 2);
-  };
-
-  SECTION("num is 3") {
-    int num = one + 2;
-    REQUIRE(num == 3);
-  };
-
-  // each section runs the setup code independently to ensure that they don't
-  // affect each other
-}
-
-// You must write 5 unique, meaningful tests for credit on the testing section
-// of this project!
-
-// See the following for an example of how to easily test your output.
-// This uses C++ "raw strings" and assumes your PageRank outputs a string with
-//   the same thing you print.
-TEST_CASE("Example PageRank Output Test", "[flag]") {
-  // the following is a "raw string" - you can write the exact input (without
-  //   any indentation!) and it should work as expected
-  string input = R"(7 2
-google.com gmail.com
-google.com maps.com
-facebook.com ufl.edu
-ufl.edu google.com
-ufl.edu gmail.com
-maps.com facebook.com
-gmail.com maps.com)";
-
-  string expectedOutput = R"(facebook.com 0.20
-gmail.com 0.20
-google.com 0.10
-maps.com 0.30
-ufl.edu 0.20
+TEST_CASE("Handles graph with single node", "[single_node]") {
+    string input = R"(1 0
+a.com b.com
+)";
+    string expectedOutput = R"(a.com 1.00
 )";
 
-  string actualOutput;
+    string actualOutput;
 
-  // somehow pass your input into your AdjacencyList and parse it to call the
-  // correct functions, for example:
-  //  AdjacencyList g;
-  //  g.parseInput(input)
-  //  actualOutput = g.getStringRepresentation()
+    // AdjacencyList g;
+    // g.parseInput(input);
+    // actualOutput = g.getStringRepresentation();
 
-  REQUIRE(actualOutput == expectedOutput);
+    REQUIRE(actualOutput == expectedOutput);
+}
+
+TEST_CASE("Handles graph with dead end node", "[dead_end]") {
+    string input = R"(2 1
+a.com b.com
+)";
+    string expectedOutput = R"(a.com 0.50
+b.com 0.50
+)";
+
+    string actualOutput;
+
+    // AdjacencyList g;
+    // g.parseInput(input);
+    // actualOutput = g.getStringRepresentation();
+
+    REQUIRE(actualOutput == expectedOutput);
+}
+
+TEST_CASE("PageRank redistributes properly after iterations", "[iterations]") {
+    string input = R"(3 3
+a.com b.com
+b.com c.com
+c.com a.com
+)";
+    string expectedOutput = R"(a.com 0.33
+b.com 0.33
+c.com 0.33
+)";
+
+    string actualOutput;
+
+    // AdjacencyList g;
+    // g.parseInput(input);
+    // g.runIterations(100);
+    // actualOutput = g.getStringRepresentation();
+
+    REQUIRE(actualOutput == expectedOutput);
+}
+
+TEST_CASE("Graph with multiple outgoing edges", "[multi_outgoing]") {
+    string input = R"(3 2
+a.com b.com
+a.com c.com
+)";
+    string expectedOutput = R"(a.com 0.15
+b.com 0.425
+c.com 0.425
+)";
+
+    string actualOutput;
+
+    // AdjacencyList g;
+    // g.parseInput(input);
+    // actualOutput = g.getStringRepresentation();
+
+    REQUIRE(actualOutput == expectedOutput);
+}
+
+// Nicholas Farr: UFID 17993779
+TEST_CASE("Handles disconnected graph", "[disconnected]") {
+    string input = R"(4 2
+a.com b.com
+c.com d.com
+)";
+    string expectedOutput = R"(a.com 0.25
+b.com 0.25
+c.com 0.25
+d.com 0.25
+)";
+
+    string actualOutput;
+
+    // AdjacencyList g;
+    // g.parseInput(input);
+    // actualOutput = g.getStringRepresentation();
+
+    REQUIRE(actualOutput == expectedOutput);
 }
