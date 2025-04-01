df88c7f43c35b0896f2898d58ea764ee0d9508ac
Nicholas Farr <nickfarr10@gmail.com>
2025-03-31 23:19:43 -0400

    created 5 catch2 tests for project checkin

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/nickfarr/Desktop/DSA/pagerank
+# Build directory: /Users/nickfarr/Desktop/DSA/pagerank/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/Users/nickfarr/Desktop/DSA/pagerank/cmake-build-debug/Tests-b12d07c_include.cmake")
+subdirs("_deps/catch2-build")
